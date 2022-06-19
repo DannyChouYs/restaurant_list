b8e04196569ed9d0e469149adbb5b67dffdbfaae
ericx790101 <ericx790101@gmail.com>
2022-06-19 12:28:36 +0800

    實作搜尋功能(利用餐廳名稱、餐廳類別進行搜尋)

--- app.js
+++ app.js
@@ -13,13 +13,27 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
+// index
 app.get('/', (req, res) => {
     res.render('index', { restaurants: restaurantList.results });
 });
 
+// show
 app.get('/restaurants/:restaurant_id', (req, res) => {
     const restaurant = restaurantList.results.find((item) => item.id.toString() === req.params.restaurant_id);
     res.render('show', { restaurant: restaurant });
+});
+
+// search
+app.get('/search', (req, res) => {
+    const keyword = req.query.keyword;
+    const restaurants = restaurantList.results.filter((item) => {
+        return (
+            item.name.toLowerCase().includes(keyword.toLowerCase()) ||
+            item.category.toLowerCase().includes(keyword.toLowerCase())
+        );
+    });
+    res.render('index', { restaurants: restaurants, keyword: keyword });
 });
 
 app.listen(port, () => {
