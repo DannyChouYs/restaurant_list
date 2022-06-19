f87446813a1948f7ac51a5e430db44a05b7f8797
ericx790101 <ericx790101@gmail.com>
2022-06-19 12:12:12 +0800

    index頁面，帶入json資料

--- app.js
+++ app.js
@@ -5,15 +5,18 @@ const port = 3000;
 
 const exphbs = require('express-handlebars');
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars')
+// load json
+const restaurantList = require('./restaurant.json');
 
-app.use(express.static('public'))
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.render('index')
-})
+    res.render('index', { restaurants: restaurantList.results });
+});
 
 app.listen(port, () => {
-    console.log(`express is running in localhost:${port}`)
-})
+    console.log(`express is running in localhost:${port}`);
+});
