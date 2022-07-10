20841f8094331131b3d09021b9704b4d71c3de2c
ericx790101 <ericx790101@gmail.com>
2022-07-10 21:19:38 +0800

    feat: 新增餐廳資料

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
 const Restaurant = require('./models/restaurant'); //載入 restaurant model
-
 // 引用 body-parser
+const bodyParser = require('body-parser');
 
 const app = express();
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }); // 設定連線到 mongoDB
@@ -27,6 +27,8 @@ const restaurantList = require('./restaurant.json');
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
 
 // index首頁
@@ -35,8 +37,25 @@ app.get('/', (req, res) => {
         .lean()
         .then((restaurants) => res.render('index', { restaurants }))
         .catch((error) => console.error(error)); // 錯誤處理
-    // res.render('index', { restaurants: restaurantList.results });
 });
+
+// 新增一家餐廳
+app.get('/restaurants/new', (req, res) => {
+    const columns = ['name', 'name_en', 'category', 'image', 'location', 'phone', 'google_map', 'description'];
+    return res.render('new', { columns });
+});
+
+// Create
+app.post('/restaurants', (req, res) => {
+    const { name, name_en, category, image, location, phone, google_map, description }= req.body; // 從 req.body 拿出表單裡的資料
+    return Restaurant.create({ name, name_en, category, image, location, phone, google_map, description }) // 存入資料庫
+        .then(() => res.redirect('/')) // 新增完成後導回首頁
+        .catch((error) => console.log(error));
+});
+
+// 修改餐廳資訊
+
+// 刪除一家餐廳
 
 // show
 app.get('/restaurants/:id', (req, res) => {
@@ -61,12 +80,6 @@ app.get('/search', (req, res) => {
     });
     res.render('index', { restaurants: restaurants, keyword: keyword });
 });
-
-// 新增一家餐廳
-
-// 修改餐廳資訊
-
-// 刪除一家餐廳
 
 app.listen(port, () => {
     console.log(`express is running in localhost:${port}`);
