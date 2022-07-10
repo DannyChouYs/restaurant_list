5d38edb857238d200da8951a909ad641efe64630
ericx790101 <ericx790101@gmail.com>
2022-07-10 16:46:32 +0800

    feat: 修改首頁獲取資料的方式

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
+const Restaurant = require('./models/restaurant'); //載入 restaurant model
+
+// 引用 body-parser
+
 const app = express();
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }); // 設定連線到 mongoDB
 
 const port = 3000;
 
-const exphbs = require('express-handlebars');
+// 取得資料庫連線狀態
+const db = mongoose.connection;
+// 連線異常
+db.on('error', () => {
+    console.log('mongodb error!');
+});
+// 連線成功
+db.once('open', () => {
+    console.log('mongodb connected!');
+});
 
 // load json
 const restaurantList = require('./restaurant.json');
@@ -13,9 +29,13 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static('public'));
 
-// index
+// index首頁
 app.get('/', (req, res) => {
-    res.render('index', { restaurants: restaurantList.results });
+    Restaurant.find()
+        .lean()
+        .then((restaurants) => res.render('index', { restaurants }))
+        .catch((error) => console.error(error)); // 錯誤處理
+    // res.render('index', { restaurants: restaurantList.results });
 });
 
 // show
@@ -35,6 +55,12 @@ app.get('/search', (req, res) => {
     });
     res.render('index', { restaurants: restaurants, keyword: keyword });
 });
+
+// 新增一家餐廳
+
+// 修改餐廳資訊
+
+// 刪除一家餐廳
 
 app.listen(port, () => {
     console.log(`express is running in localhost:${port}`);
